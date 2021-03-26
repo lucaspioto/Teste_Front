d22c91ab87fcf40a01b2ba20edb7726b433afdc2
Lucas Tamagnone <lucaspioto@gmail.com>
2021-03-25 23:22:17 -0300

    menu mobile

--- js/components/add-smooth-scroll.js
+++ js/components/add-smooth-scroll.js
@@ -70,3 +70,13 @@ window.addEventListener('load', () => {
   new SmoothScroll();
 
 });
+
+document.querySelector(".toggle-menu").addEventListener("click", function () {
+  document.querySelector("#sidemenu").classList.add('do-show');
+  document.querySelector('body').classList.add('menu-open');
+});
+
+document.querySelector(".toggle-menu.close").addEventListener("click", function () {
+  document.querySelector("#sidemenu").classList.remove('do-show');
+  document.querySelector('body').classList.remove('menu-open');
+}); 
